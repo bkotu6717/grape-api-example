079616d64767bb69e59181a1cb3f5345933198c2
Bhaskara Rao Kotu <bhaskara.rao@introhive.com>
2017-08-18 16:13:58 +0530

    Initialize app with grape success grape api call

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Graduate.delete_all
+graduates = []
+100.times { |i| graduates << { name: "Graduate #{i + 1}"} }
+
+Graduate.create! graduates
